918e1c738a55439da23b48cc8c0063ca8dbc10cd
Dencyuman <takotakocyuman@gmail.com>
2024-02-21 00:56:42 +0900

    fix shell

--- scripts/setup-reverse-proxy.sh
+++ scripts/setup-reverse-proxy.sh
@@ -9,10 +9,10 @@ read -p "Enter your server_name (domain or public IP): " server_name
 
 # Nginxのインストール
 echo "Installing Nginx..."
-brew install nginx
+sudo yum install -y nginx
 
 # Nginx設定ファイルの作成
-NGINX_CONF="/home/linuxbrew/.linuxbrew/etc/nginx/servers/my_fastapi_app.conf"
+NGINX_CONF="/etc/nginx/conf.d/my_fastapi_app.conf"
 echo "server {
     listen 80; # 外部からのアクセスを受けるポートを80に設定
     server_name $server_name;  # ユーザー入力を反映
@@ -24,15 +24,15 @@ echo "server {
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
     }
-}" | tee $NGINX_CONF
+}" | sudo tee $NGINX_CONF
 
 # Nginxの設定をテスト
 echo "Testing Nginx configuration..."
-nginx -t
+sudo nginx -t
 
 # Nginxを再起動して設定を適用
 echo "Restarting Nginx..."
-brew services restart nginx
+sudo systemctl restart nginx
 
 echo "Nginx has been configured successfully."
 echo "You can access your FastAPI app at http://$server_name"
