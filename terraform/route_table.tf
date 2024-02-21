d6c38e3b44d132f9e3d8b2cc7338cd7afda541b7
Dencyuman <takotakocyuman@gmail.com>
2024-02-21 17:56:56 +0900

    remove natgw

--- terraform/route_table.tf
+++ terraform/route_table.tf
@@ -30,11 +30,11 @@ resource "aws_route_table" "private" {
   }
 }
 
-resource "aws_route" "private" {
-  route_table_id = aws_route_table.private.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.main.id
-}
+#resource "aws_route" "private" {
+#  route_table_id = aws_route_table.private.id
+#  destination_cidr_block = "0.0.0.0/0"
+#  nat_gateway_id = aws_nat_gateway.main.id
+#}
 
 resource "aws_route_table_association" "private1" {
   subnet_id = aws_subnet.private1.id
