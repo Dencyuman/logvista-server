6b24a5159a4d91480c59ca5a4205eafdc8406438
Dencyuman <takotakocyuman@gmail.com>
2024-02-21 15:15:58 +0900

    fix rds terraform

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,12 +1,12 @@
 resource "aws_db_instance" "main" {
   allocated_storage = 20
   engine = "postgres"
-  engine_version = "16.1"
+  engine_version = "12.17"
   instance_class = "db.t3.micro"
   db_name = var.db_name
   username = var.db_username
   password = var.db_password
-  parameter_group_name = "default.postgres16"
+  parameter_group_name = "default.postgres12"
   skip_final_snapshot = true
 
   vpc_security_group_ids = [aws_security_group.allow_postgres.id]
